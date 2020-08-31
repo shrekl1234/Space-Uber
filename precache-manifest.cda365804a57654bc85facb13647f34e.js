2f1696f017f5fd266d1e95d81a274ef2fefdc3d3
shrekl1234 <shrekl1234@users.noreply.github.com>
2020-08-31 08:38:37 +0530

    Updates

---
+++ precache-manifest.cda365804a57654bc85facb13647f34e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8fcbcb94310a8a0ab63273ef736990",
+    "url": "/Space-Uber/index.html"
+  },
+  {
+    "revision": "6f22725aacd7de5df036",
+    "url": "/Space-Uber/static/css/main.29c7cbc8.chunk.css"
+  },
+  {
+    "revision": "aaec0fae4844a09c82e1",
+    "url": "/Space-Uber/static/js/2.32e34bc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Space-Uber/static/js/2.32e34bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f22725aacd7de5df036",
+    "url": "/Space-Uber/static/js/main.35efb8d8.chunk.js"
+  },
+  {
+    "revision": "a09b257db2e4e945a178",
+    "url": "/Space-Uber/static/js/runtime-main.6c97c2c1.js"
+  }
+]);
